0841328881196d08dbf4809b4b651005069e9114
Josh Pollock <josh@calderawp.com>
2019-09-03 11:46:23 -0400

    Select field

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,37 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
-import { useDomEvent } from "framer-motion";
 import "./styles.css";
-import { useEffectOnce } from "react-use";
 import { useInterval } from "react-use";
 import { useDomInput } from "./useDomInput";
+import { useDomSelect } from "./useDomSelect";
 
 function App() {
   const [textValue, setTextValue] = useState("Default From React");
+  const [selectValue, setSelectValue] = useState("smol");
+
+  const [selectFieldRef] = useDomSelect({
+    elementId: "size",
+    value: selectValue,
+    handler: setSelectValue
+  });
   const [textInputRef] = useDomInput({
     elementId: "firstName",
-    value: textValue,
+    value: selectValue,
     handler: setTextValue
   });
 
   //Change input field state
   useInterval(() => {
     setTextValue("Change by React State");
+    setSelectValue("smol");
   }, 10000);
 
   return (
     <div className="App">
       <h2>React App</h2>
       <p>{textValue}</p>
+      <p>{selectValue}</p>
+
       <p>State is reset to "RESET!" every 10 seconds</p>
     </div>
   );
---
+++ src/useDomSelect.tsx
@@ -0,0 +1,39 @@
+import React, { useRef, useEffect } from "react";
+import { useEffectOnce } from "react-use";
+
+interface EventTarget {
+  event: {
+    target: { value: string };
+  };
+}
+
+function inputChangeHandler(event: HTMLInputElement, handler: EventListener) {
+  return handler(event.target.value);
+}
+
+export const useDomSelect = (props: {
+  elementId: string;
+  value: string;
+  handler?: EventListener | undefined;
+}) => {
+  const { elementId, value, handler } = props;
+  const inputRef = useRef(null);
+
+  //Bind change handler on mount/ unmount
+  useEffectOnce(() => {
+    inputRef.current = document.getElementById(elementId);
+    handler(inputRef.current.value);
+    const callback = e => inputChangeHandler(e, handler);
+    inputRef.current.addEventListener("keypress", callback, true);
+    return () => {
+      inputRef.current.removeEventListener("keypress", callback, true);
+    };
+  });
+
+  //Bind value of input to React state
+  useEffect(() => {
+    inputRef.current.value = value;
+  });
+
+  return [inputRef];
+};
