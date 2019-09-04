003394c34042bbc0a7439009af724bfee6d258db
Josh Pollock <jpollock412@gmail.com>
2019-09-04 16:48:45 -0400

    chore(src/demo): demo with parcel
    Moved index.tsx to Demo.js and installed parcel

---
+++ src/Demo.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import './styles.css';
+import { useInterval } from 'react-use';
+import { useDomInput } from './useDomInput';
+import { useDomSelect } from './useDomSelect';
+import { useDomCheckboxGroup } from './useDomCheckboxGroup';
+import { useDomRadioGroup } from './useDomRadioGroup';
+function mapToJson(map) {
+  return JSON.stringify([...map]);
+}
+function jsonToMap(jsonStr) {
+  return new Map(JSON.parse(jsonStr));
+}
+
+/**
+ * Full demonstration component
+ */
+export default function Demo() {
+  const [textValue, setTextValue] = useState('Default From React');
+  const [selectValue, setSelectValue] = useState('smol');
+
+  const [checkboxState, setCheckboxState] = useState(
+    new Map().set('fruitJuicesOrange', true),
+  );
+
+  const [radioValue, setRadioValue] = useState('green');
+
+  const [selectFieldRef] = useDomSelect({
+    elementId: 'size',
+    value: selectValue,
+    handler: setSelectValue,
+  });
+  const [textInputRef] = useDomInput({
+    elementId: 'firstName',
+    value: textValue,
+    handler: setTextValue,
+  });
+
+  const [boxesRef] = useDomCheckboxGroup({
+    name: 'fruitJuices',
+    value: checkboxState,
+    handler: setCheckboxState,
+  });
+
+  useDomRadioGroup({
+    name: 'color',
+    value: radioValue,
+    handler: setRadioValue,
+  });
+
+  //Change input field state
+  useInterval(() => {
+    setTextValue('Reset');
+    setSelectValue('smol');
+  }, 10000);
+
+  return (
+    <div className="App">
+      <h2>React App</h2>
+      <p>{textValue}</p>
+      <p>{selectValue}</p>
+      <p>{mapToJson(checkboxState)}</p>
+      <p>{radioValue}</p>
+      <p>
+        State for text will reset to "RESET!" every 10 seconds. Select will set
+        to "large"
+      </p>
+    </div>
+  );
+}
